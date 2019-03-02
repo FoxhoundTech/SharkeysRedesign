b761b575f8d3db87ba94a779666492dbb4d991bd
jkolts <45447182+jkolts16@users.noreply.github.com>
2019-03-02 18:03:44 -0500

    Update menu.php

--- menu.php
+++ menu.php
@@ -15,7 +15,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 		<link rel="stylesheet" href="sharkeys.css" type='text/css'>
-		<style>
+		<style> <!-- Trying to change the size of images for the image carousel -->
 			.carousel-inner{
 				height: 300px;
 			}			
@@ -29,6 +29,7 @@
 	<body>
 		<?php require_once("navbar.php"); ?>
 		
+		<!-- Image Carousel -->
 		<div id="menuCarousel" class="carousel slide pt-5" data-ride="carousel">
 			<div class="carousel-inner">
 				<!--<div class="carousel-item active">
@@ -53,6 +54,8 @@
 		<div class="pt-5 text-center">
 			<h1 style="color: white">MENU</h1>
 		</div>
+		
+		<!--Menu Accordion -->
 		</div>
 		<div id="accordion" style="padding-top: 2%;padding-left: 10%; padding-right: 10%; padding-bottom: 2%">
 			
